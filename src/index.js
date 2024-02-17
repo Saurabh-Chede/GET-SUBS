3fc1f0b5931e2cb886082c4d2059e0357e61347b
Saurabh-Chede <saurabhchede21@gmail.com>
2024-02-17 15:50:35 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
+// import require modules
 const express = require("express");
 const app = require("./app.js");
 
 const mongoose = require("mongoose");
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Connect to DATABASE
-// const DATABASE_URL = "mongodb://127.0.0.1:27017/subscribers";
 
 const DATABASE_URL = "mongodb+srv://saurabhchede21:zjz1wDbf29iYqX3n@cluster0.cecuypn.mongodb.net/";
 
@@ -20,8 +20,12 @@ mongoose.connect(DATABASE_URL, {
 
 const db = mongoose.connection;
 
+// If an error occurs during connection, handle and log the error
 db.on("error", (err) => console.log(err));
+
+// If the connection is successful, log a success message
 db.once("open", () => console.log("connected to database"));
 
-// Start Server
+// Start Server and make it listen on the specified port
+// A message will be printed to the console once the server is up and running
 app.listen(port, () => console.log(`App listening on port ${port}!`));
